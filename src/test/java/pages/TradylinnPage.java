10c2ba8150bdaea62bb4f3a52f9c073c5b2ac8de
ahmet portakal <ahmettportakal27@gmail.com>
2022-06-24 01:21:14 +0300

    testNG

--- src/test/java/pages/TradylinnPage.java
+++ src/test/java/pages/TradylinnPage.java
@@ -5,43 +5,94 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.Random;
+
 public class TradylinnPage {
     public TradylinnPage() {
 
         PageFactory.initElements(Driver.getDriver(), this);
-    }
 
-    @FindBy(xpath = "//*[text()='Hesabım']")
-    public WebElement hesabımLogınTıkla;
-    @FindBy (xpath = "//input[@id='username']")
-    public WebElement eMailAdress;
+    }
+    public Random rnd = new Random();
+    public String codeID= String.valueOf(rnd.nextInt(10000));
+
+    @FindBy(xpath = "//a[@class='login inline-type']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement username;
+
     @FindBy(xpath = "//input[@id='password']")
     public WebElement password;
-    @FindBy(xpath = "//button[text()='Giriş Yap']")
-    public WebElement girişYapBtn;
+
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement loginButton2;
+
+    @FindBy(xpath = "(//*[text()='Hesabım'])[1]")
+    public WebElement hesabimButton;
 
     @FindBy(xpath = "//*[text()='Store Manager']")
-    public WebElement storeManager;
+    public WebElement storeManagerButton;
+
     @FindBy(xpath = "(//span[@class='text'])[6]")
     public WebElement kuponlar;
+
     @FindBy(xpath = "//*[text()='Yeni ekle']")
     public WebElement yeniEkle;
+
+    @FindBy(xpath = "//input[@class='wcfm-text wcfm_ele']")
+    public WebElement code;
+
     @FindBy (xpath = "//*[text()='Restriction']")
     public WebElement restrictionTıkla;
+
     @FindBy(xpath = "//input[@id='minimum_amount']")
     public WebElement minumumSpend;
+
     @FindBy(xpath = "//input[@id='maximum_amount']")
     public WebElement maxSpend;
+
     @FindBy (xpath = "//input[@id='individual_use']")
     public WebElement ındividualCheckBox;
+
     @FindBy(xpath = "//input[@id='exclude_sale_items']")
     public WebElement excludeCheckBox;
+
+    @FindBy(xpath = "(//input[@class='select2-search__field'])[4]")
+    public WebElement ddp;
+
+    @FindBy(xpath = "//input[@id='wcfm_coupon_manager_submit_button']")
+    public WebElement submitTıkla;
+
+
+//15 limit kısmı
     @FindBy(xpath = "//div[@id='coupons_manage_limit']")
     public WebElement limitUygula;
+    @FindBy(xpath ="//input[@id='usage_limit']")
+    public WebElement usageLimitCoupon;
+    @FindBy(xpath = "//input[@id='limit_usage_to_x_items']")
+    public WebElement usLimitİtems;
+    @FindBy(xpath = "//input[@id='usage_limit_per_user']")
+    public WebElement perUser;
 
 
 
 
+//13 kupon ekleme kısmı
+    //@FindBy(xpath = "//input[@class='wcfm-text wcfm_ele']")
+    //public WebElement code;
+    @FindBy(xpath = "//textarea[@id='description']")
+    public WebElement description;
+    @FindBy(xpath = "//select[@id='discount_type']")
+    public WebElement percent;
+    @FindBy(xpath = "//input[@id='coupon_amount']")
+    public WebElement amount;
+    @FindBy(xpath = "//input[@id='expiry_date']")
+    public WebElement expirydate;
+    @FindBy (xpath = "//input[@id='free_shipping']")
+    public WebElement freeCheckBox;
+    @FindBy(xpath = "(//input[@type='checkbox'])[2]")
+    public WebElement showOnCheck;
 
 
 
--- src/test/java/tests/practıce/TradylinnTest.java
+++ src/test/java/tests/practıce/TradylinnTest.java
@@ -1,6 +1,7 @@
 package tests.practıce;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,7 +10,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import pages.TradylinnPage;
+import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusablaMethods;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
@@ -17,38 +20,60 @@ import java.time.Duration;
 public class TradylinnTest extends TestBaseRapor {
     @Test
     public void testTradylinn() throws InterruptedException {
+        Actions actions = new Actions(Driver.getDriver());
         extentTest = extentReports.createTest("cupon testi", "cuponlara kısıt getirebilmeli");
-        TradylinnPage tdPage = new TradylinnPage();
-        Driver.getDriver().get("https://Tradylinn.com/wp_admin");
+        TradylinnPage portiPage = new TradylinnPage();
 
-        tdPage.hesabımLogınTıkla.click();
-        tdPage.eMailAdress.sendKeys("umitenes89@gmail.com");
-        tdPage.password.sendKeys("Ahmet27,.");
-        Actions actions=new Actions(Driver.getDriver());
-        //actions.sendKeys(Keys.PAGE_DOWN).perform();
-        tdPage.girişYapBtn.click();
-        Thread.sleep(3000);
+        Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
+        extentTest.info("Tradylinn ana sayfasına gidildi");
 
-        tdPage.storeManager.click();
+        portiPage.loginButton.click();
+        extentTest.info("Login'e tiklandi");
+
+        portiPage.username.sendKeys(ConfigReader.getProperty("correctEmail"));
+        portiPage.password.sendKeys(ConfigReader.getProperty("correctPassword"));
+        portiPage.loginButton2.click();
+        extentTest.info("Geçerli mail ve şifre girildi ve giriş yapıldı");
+        Thread.sleep(5000);
+        portiPage.hesabimButton.click();
+        extentTest.info("Hesabim butonuna basildi");
+        portiPage.storeManagerButton.click();
+        extentTest.info("Store manager butonuna tiklandi.");
+
         actions.sendKeys(Keys.PAGE_DOWN).perform();
-        tdPage.kuponlar.click();
-        tdPage.yeniEkle.click();
+        portiPage.kuponlar.click();
+        portiPage.yeniEkle.click();
+        portiPage.code.sendKeys("500");
         actions.sendKeys(Keys.PAGE_DOWN).perform();
-        Thread.sleep(3000);
-        tdPage.restrictionTıkla.click();
-        Thread.sleep(3000);
+       // WebElement newCoupon=Driver.getDriver().findElement(By.xpath("//*[text()='"+portiPage.codeID+"']"));
+        ReusablaMethods.waitFor(10);
+        portiPage.minumumSpend.sendKeys("3");
+        portiPage.maxSpend.sendKeys("5");
+       // actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+        JavascriptExecutor js2= (JavascriptExecutor) Driver.getDriver();
+        js2.executeScript("arguments[0].click();",portiPage.ındividualCheckBox);
+        js2.executeScript("arguments[0].click();",portiPage.excludeCheckBox);
+        //portiPage.ındividualCheckBox.click();
+       // portiPage.excludeCheckBox.click();
 
-        tdPage.minumumSpend.sendKeys("3");
-        tdPage.maxSpend.sendKeys("5");
-        actions.sendKeys(Keys.PAGE_DOWN).perform();
-        tdPage.ındividualCheckBox.click();
-        tdPage.excludeCheckBox.click();
+       // JavascriptExecutor jse= (JavascriptExecutor) Driver.getDriver();
+        //WebElement signInButonu= Driver.getDriver().findElement(By.xpath("(//span[text()='Sign in'])[2]"));
+        //Thread.sleep(3000);
+        //jse.executeScript("arguments[0].scrollIntoView(true);",signInButonu);
+        // sign in butonuna js ile click yapalim
+        //jse.executeScript("arguments[0].click();",signInButonu);
+        //Thread.sleep(3000);
 
+       // WebElement ddp=Driver.getDriver().findElement(By.xpath("(//input[@class='select2-search__field'])[4]"));
+       // JavascriptExecutor js1= (JavascriptExecutor) Driver.getDriver();
+        //js1.executeScript("arguments[0].click();",portiPage.ddp);
+         //Select select = new Select(portiPage.ddp);
+         //select.selectByIndex(1);
 
+        JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click();",portiPage.submitTıkla);
+        //portiPage.submitTıkla.click();
 
-        WebElement ddp=Driver.getDriver().findElement(By.xpath("(//input[@class='select2-search__field'])[4]"));
-        Select select = new Select(ddp);
-         select.selectByIndex(1);
 
 
     }
